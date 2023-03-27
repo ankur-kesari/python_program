2e446ccf0a38567811ecfc25c71ced44c90a7c19
Ankur Kesari <64275046+ankur-kesari@users.noreply.github.com>
2023-03-27 13:15:52 +0530

    Create update_mong_password.py

---
+++ update_mong_password.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+# Connect to MongoDB
+client = MongoClient('<mongo_url_string>')
+
+# # Get the database and user collection
+# db = client['mydatabase']
+# users = db['admin']
+
+# # Define the new password for the user
+# new_password = 'nfaghshikdn1dagh'
+
+# # Update the user's password
+# users.update_user(
+#     'admin',
+#     password=new_password,
+#     mechanism='SCRAM-SHA-256'
+# )
+db = client.get_database()
+print(db)
